2e470a25b44048e11890bdb640a19da9c1f129a8
Michael Heilmann <mheilman@ist.ucf.edu>
2017-01-26 14:04:22 -0500

    merging halcyon branch into master.  Old code will live in opensim branch

---
+++ Node/Node.py
@@ -0,0 +1,299 @@
+'''
+Created on Jan 30, 2013
+
+@author: mheilman
+'''
+
+import cherrypy
+
+from Region import Region
+import time, uuid, requests, json, threading, os, shutil
+from HostMonitor import HostMonitor
+from cherrypy.process.plugins import Monitor
+import cherrypy
+from cherrypy.lib.static import serve_file
+
+class Node:
+    ''' A management class interfacing with region worker objects '''
+
+    def __init__(self, conf):
+        self.availablePorts = []
+        self.registeredRegions = {}
+
+        statsInterval = conf['interval']
+        self.nodePort = conf['port']
+        self.key = uuid.uuid4()
+        self.host = conf['host']
+        self.frontendURI = conf['webAddress'] + ":" + conf['webPort']
+        self.frontendAddress = conf['webAddress']
+        self.binDir = conf['binDir']
+        self.regionDir = conf['regionDir']
+        self.publicAddress = conf['regionAddress']
+        for r,c in zip(conf['regionPorts'],conf['consolePorts']):
+            self.availablePorts.append(r)
+
+        self.consoleUser = conf['consoleUser']
+        self.consolePass = conf['consolePass']
+
+        #we can't start up without our master config
+        success = False
+        regions = []
+        while not success:
+            try:
+                success, regions = self.loadRemoteConfig()
+            except Exception, e:
+                print "Error contacting MGM: %s" % e
+                time.sleep(30)
+
+        self.initializeRegions(regions)
+
+        self.monitor = HostMonitor()
+
+        Monitor(cherrypy.engine, self.updateStats, frequency=statsInterval).subscribe()
+        cherrypy.engine.subscribe('stop',self.engineExit)
+
+        print "node started"
+
+    def engineExit(self):
+        #for name in self.registeredRegions:
+        #    del self.registeredRegions[name]
+        self.registeredRegions.clear()
+
+    def loadRemoteConfig(self):
+        print "loading config from MGM"
+        #load additional config from master service
+        url = "http://%s/dispatch/node" % (self.frontendURI)
+        r = requests.post(url, data={'host':self.host, 'port':self.nodePort, 'key':self.key, 'slots': len(self.availablePorts)}, verify=False)
+        if not r.status_code == requests.codes.ok:
+            raise Exception("Error contacting MGM at %s" % url)
+
+        result = json.loads(r.content)
+
+        if not result["Success"]:
+            raise Exception("Error loading config: %s" % result["Message"])
+
+        if len(result['Regions']) > len(self.availablePorts):
+            raise Exception("Error: too many regions for configured ports")
+        return True, result['Regions']
+
+    def initializeRegions(self, regions):
+        '''create a Regions process for each assigned region'''
+        #convert regions to a map
+        regs = {}
+        for r in regions:
+            regs[r['uuid']] = r
+        # Reuse assignments already placed on disk
+        assignments = {}
+        for id in os.listdir(self.regionDir):
+            if id not in regs:
+                # we have a region on disk that is not supposed to be ours, kill it and ignore its mapping
+                r = Region(0, id, '', self.binDir, self.regionDir, self.frontendURI, self.publicAddress, self.consoleUser, self.consolePass)
+                if r.isRunning:
+                    r.kill()
+                continue
+            halcyonFile = os.path.join(self.regionDir, id, 'Halcyon.ini')
+            if os.path.isfile(halcyonFile):
+                for line in open(halcyonFile, 'r'):
+                    if "http_listener_port" in line:
+                        assignments[id] = int(line.split('"')[1])
+
+        for id,port in assignments.iteritems():
+            self.availablePorts.remove(port)
+            region = regs[id]
+            del regs[id]
+            self.registeredRegions[id] = Region(
+                port,
+                region['uuid'],
+                region['name'],
+                self.binDir,
+                self.regionDir,
+                self.frontendURI,
+                self.publicAddress,
+                self.consoleUser,
+                self.consolePass)
+        # assign remaining regions
+        for id in regs:
+            region = regs[id]
+            port = self.availablePorts.pop(0)
+            self.registeredRegions[region['uuid']] = Region(
+                port,
+                region['uuid'],
+                region['name'],
+                self.binDir,
+                self.regionDir,
+                self.frontendURI,
+                self.publicAddress,
+                self.consoleUser,
+                self.consolePass)
+
+    def updateStats(self):
+        self.monitor.updateStatistics()
+        stats = {}
+        stats['host'] = self.monitor.stats
+        stats['processes'] = []
+
+        for id,region in self.registeredRegions.iteritems():
+            p = {}
+            p['id'] = id
+            p['running'] = region.isRunning
+            p['stats'] = region.stats
+            stats['processes'].append(p)
+
+        url = "http://%s/dispatch/stats/%s" % (self.frontendURI, self.host)
+        try:
+            r = requests.post(url, data={"json": json.dumps(stats)}, verify=False)
+        except requests.ConnectionError:
+            print "error connecting to master"
+            return
+        if not r.status_code == requests.codes.ok:
+            print "error uploading stats to master"
+        else:
+            print "%s - Upload Status: %s" % (time.strftime("%Y-%m-%d %H:%M:%S"), r.content)
+
+    @cherrypy.expose
+    def index(self):
+        return "<html><body><h1>MOSES Grid Manager Node: %s</h1></body></html>" % self.host
+
+    # FRONT END FUNCTION CALLS
+
+    @cherrypy.expose
+    def add(self, id, name):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return json.dumps({ "Success": False, "Message": "Denied, this functionality is restricted to the mgm web app"})
+
+        #check if region already present here
+        if id in self.registeredRegions:
+            print "Add region %s failed: Region already present on node" % id
+            return json.dumps({ "Success": False, "Message": "Region already exists on this Node"})
+        try:
+            port = self.availablePorts.pop(0)
+        except Exception, e:
+            print "Add region %s failed: No Available Ports" % id
+            return json.dumps({ "Success": False, "Message": "No slots remaining"})
+        self.registeredRegions[id] = Region(
+            port,
+            id,
+            name,
+            self.binDir,
+            self.regionDir,
+            self.frontendURI,
+            self.publicAddress,
+            self.consoleUser,
+            self.consolePass)
+        print "Add region %s succeeded" % id
+        return json.dumps({ "Success": True})
+
+    @cherrypy.expose
+    def remove(self, id):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return json.dumps({ "Success": False, "Message": "Denied, this functionality is restricted to the mgm web app"})
+
+        #find region, and remove if found
+        if not id in self.registeredRegions:
+            print "Remove region %s failed: Region not present on node" % id
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        if self.registeredRegions[id].isRunning:
+            print "Remove region %s failed: Region is currently running" % id
+            return json.dumps({ "Success": False, "Message": "Region is still running"})
+        port = self.registeredRegions[id].port
+        del self.registeredRegions[id]
+        self.availablePorts.insert(0,port)
+        print "Remove region %s succeeded" % id
+        return json.dumps({ "Success": True})
+
+    @cherrypy.expose
+    def start(self, id):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return json.dumps({ "Success": False, "Message": "Denied, this functionality is restricted to the mgm web app"})
+        if not id in self.registeredRegions:
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        self.registeredRegions[id].start()
+        return json.dumps({ "Success": True})
+
+    @cherrypy.expose
+    def stop(self, id):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return json.dumps({ "Success": False, "Message": "Denied, this functionality is restricted to the mgm web app"})
+        if not id in self.registeredRegions:
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        self.registeredRegions[id].stop()
+        return json.dumps({ "Success": True})
+
+    @cherrypy.expose
+    def kill(self, id):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return json.dumps({ "Success": False, "Message": "Denied, this functionality is restricted to the mgm web app"})
+        if not id in self.registeredRegions:
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        self.registeredRegions[id].kill()
+        return json.dumps({ "Success": True})
+
+    @cherrypy.expose
+    def saveOar(self, id, job):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return "Denied, this functionality is restricted to the mgm web app"
+
+        if not id in self.registeredRegions:
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        if not self.registeredRegions[id].isRunning:
+            return json.dumps({ "Success": False, "Message": "Region must be running to manage oars"})
+
+        report = "http://%s/task/report/%s" % (self.frontendURI, job)
+        upload = "http://%s/task/upload/%s" % (self.frontendURI, job)
+        if self.registeredRegions[id].saveOar(report, upload):
+            return json.dumps({ "Success": True})
+        return json.dumps({ "Success": False, "Message": "An error occurred communicating with the region"})
+
+    @cherrypy.expose
+    def loadOar(self, id, job):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return "Denied, this functionality is restricted to the mgm web app"
+
+        if not id in self.registeredRegions:
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        if not self.registeredRegions[id].isRunning:
+            return json.dumps({ "Success": False, "Message": "Region must be running to manage oars"})
+
+        ready = "http://%s/task/ready/%s" % (self.frontendURI, job)
+        report = "http://%s/task/report/%s" % (self.frontendURI, job)
+        if self.registeredRegions[id].loadOar(ready, report):
+            return json.dumps({ "Success": True})
+        return json.dumps({ "Success": False, "Message": "An error occurred communicating with the region"})
+
+    @cherrypy.expose
+    def consoleCmd(self, id, cmd):
+        #veryify request is coming from the web frontend
+        ip = cherrypy.request.headers["Remote-Addr"]
+        if not ip == self.frontendAddress:
+            print "INFO: Attempted region control from ip %s instead of web frontent" % ip
+            return "Denied, this functionality is restricted to the mgm web app"
+
+        if not id in self.registeredRegions:
+            return json.dumps({ "Success": False, "Message": "Region not present"})
+        if not self.registeredRegions[id].isRunning:
+            return json.dumps({ "Success": False, "Message": "Region must be running to manage oars"})
+
+        if self.registeredRegions[id].consoleCmd(cmd):
+            return json.dumps({ "Success": True})
+        return json.dumps({ "Success": False, "Message": "An error occurred communicating with the region"})
--- mgmNode.py
+++ mgmNode.py
@@ -4,26 +4,23 @@ Created on Sep 22, 2011
 
 @author: mheilman
 '''
-import sys, os, ConfigParser
+import sys, os, cherrypy, ConfigParser
 
-from MgmNode.Slave import Slave
+from Node import Node
 
 import socket, string
 
-from OpenSSL import crypto
-
-from twisted.web import server
-from twisted.internet import reactor, ssl
+#from OpenSSL import crypto, SSL
 
 def modulePath():
     if hasattr(sys,"frozen"):
         return os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding( )))
     return os.path.dirname(unicode(__file__, sys.getfilesystemencoding( )))
-    
+
 def generateCerts(certFile, keyFile):
     k = crypto.PKey()
     k.generate_key(crypto.TYPE_RSA, 1024)
-    
+
     cert = crypto.X509()
     cert.get_subject().C = "US"
     cert.get_subject().ST = "Florida"
@@ -37,7 +34,7 @@ def generateCerts(certFile, keyFile):
     cert.set_issuer(cert.get_subject())
     cert.set_pubkey(k)
     cert.sign(k, 'sha1')
-    
+
     open(certFile, "wt").write(
         crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
     open(keyFile, "wt").write(
@@ -53,26 +50,84 @@ def loadConfig(filePath):
     conf['binDir'] = config.get('node','opensim_template')
     conf['regionDir'] = config.get('node','region_dir')
     conf['webAddress'] = config.get('node', 'mgm_address')
-    conf['certFile'] = config.get('ssl', 'cert')
-    conf['keyFile'] = config.get('ssl', 'key')
+    conf['webPort'] = config.get('node', 'mgm_port')
+    #conf['certFile'] = config.get('ssl', 'cert')
+    #conf['keyFile'] = config.get('ssl', 'key')
     conf['interval'] = int(config.get('node', 'sample_interval'))
-    
+    conf['consoleUser'] = config.get('halcyon', 'user')
+    conf['consolePass'] = config.get('halcyon', 'pass')
+
     portRange = config.get('node','region_port_range')
     consoleRange = config.get('node','console_port_range')
-    
+
     vals = string.split(portRange,'-')
     conf['regionPorts'] = range(int(vals[0]),int(vals[1])+1)
-        
+
     vals = string.split(consoleRange,'-')
     conf['consolePorts'] = range(int(vals[0]),int(vals[1])+1)
 
     return conf
-            
 
-os.chdir(modulePath())
-conf = loadConfig(os.path.join(modulePath() ,'mgm.cfg'))
-if not os.path.isfile(conf['certFile']) or not os.path.isfile(conf['keyFile']):
-    generateCerts(conf['certFile'], conf['keyFile'])
-site = server.Site(Slave(conf))
-reactor.listenSSL(conf['port'], site, ssl.DefaultOpenSSLContextFactory(conf['keyFile'],conf['certFile']))
-reactor.run()
+'''
+if sys.platform == "win32":
+    import win32serviceutil, win32service
+    class NodeService(win32serviceutil.ServiceFramework):
+        _svc_name_ = "MGMNode"
+        _svc_display_name_ = "MGM Host Node"
+
+        def SvcStop(self):
+            self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+            cherrypy.engine.exit()
+            self.ReportServiceStatus(win32service.SERVICE_STOPPED)
+
+        def SvcDoRun(self):
+            self.ReportServiceStatus(win32service.SERVICE_RUNNING)
+            localPath = modulePath()
+            conf = loadConfig(os.path.join(localPath,'mgm.cfg'))
+            app = Slave(conf)
+            cherrypy.tree.mount(app, '/', config={'/': {}})
+            cherrypy.config.update({
+                'global':{
+                    'server.socket_host':'0.0.0.0',
+                    'server.socket_port':conf['port'],
+                    'log.screen': True,
+                    'log.error_file': os.path.join(modulePath(),"error.log"),
+                    'log.access_file': os.path.join(modulePath(),"access.log"),
+                    'engine.autoreload.on': False,
+                    'engine.SIGHUP': None,
+                    'engine.SIGTERM': None
+                }
+            })
+            cherrypy.engine.start()
+            cherrypy.engine.block()
+'''
+
+def start():
+    conf = loadConfig(os.path.join(modulePath() ,'mgm.cfg'))
+    app = Node(conf)
+    #if not os.path.isfile(conf['certFile']) or not os.path.isfile(conf['keyFile']):
+    #    generateCerts(conf['certFile'], conf['keyFile'])
+    cherrypy.config.update({
+        'global':{
+            'server.socket_host':'0.0.0.0',
+            'server.socket_port':conf['port'],
+            'log.screen': True,
+            'engine.autoreload.on': False,
+            'engine.SIGHUP': None,
+            'engine.SIGTERM': None,
+            'server.max_request_body_size': 0,  # disable file size limits, dangerous
+            #'server.ssl_module': 'pyopenssl',
+            #'server.ssl_certificate':conf['certFile'],
+            #'server.ssl_private_key':conf['keyFile']
+        }
+    })
+    cherrypy.quickstart(app, config={'/': {}})
+
+if __name__ == '__main__':
+    #if len(sys.argv) > 1 and sys.argv[1] == "test":
+    start()
+    #else:
+    #    if sys.platform == "win32":
+    #        win32serviceutil.HandleCommandLine(NodeService)
+    #    else:
+    #        start()
